2218299b2ef5c0c7168f743e86a8561ad28309d3
Aecio_Lucio <aeciofilho12@gmail.com>
2023-04-01 20:06:53 -0300

    Limpa limpa limpa

--- teste.js
+++ teste.js
@@ -22,6 +22,7 @@ const inputs = {}
 const inputElements = document.querySelectorAll('input')
 inputElements.forEach(input => {
   input.addEventListener('input', () => {
+
     const textoArray = String(input.value).split(/(?!$)/u) 
     const textoTelaArray = String(texto).split(/(?!$)/u)
     erro = []
@@ -32,38 +33,134 @@ inputElements.forEach(input => {
       } 
     } 
 
-    async function imprimirCerto(texto){
-      textoTela.textContent += texto
-      await new Promise(resolve => setTimeout(resolve, 500));
-      compilado = ''
-    }
+    
+  ///////////////////////////////////////////////////////////////////////
 
 
-    function imprimirErro(texto){
-      const span = document.createElement('span')
-      span.classList.add('diferente')
-      span.textContent = texto
-      textoTela.appendChild(span)
-    }
+
     
 
-    let  arrayTextoCerto = textoTelaArray
-    let  arrayTextoDigitado = textoArray
-    let  arrayIndiceErrado = erro
+    arrayCerto = textoTelaArray
+    arrayDigitado = textoArray
+    arrayErrado = erro
 
-    //console.log(arrayTextoCerto,"\n",arrayTextoDigitado,"\n",arrayIndiceErrado)
-    let compilado = ''
-    for (let ip = 0 ; arrayTextoDigitado.length > ip ; ip++ ){
-      for(let i = 0; erro.length > i ; i++){
-        if ( ip != erro[i] ){
-          compilado += arrayTextoDigitado[ip]
+    // validar se o index do digitado está como o esperado
+      // se estiver certo armazena o indice do array certo no compilador e vai para o proxima letra
+      //se estiver errado compila tudo que está armazenado no array compilador, imprime o texto como certo e limpa o compilador, imprime a letra do errado
+    compilador = []
+    for (let ip = 0; arrayDigitado.length > ip; ip++ ){
+      for (let i = 0; erro.length > i; i++ ){
+        if (erro[i] != arrayCerto[ip]){
+          compilador.push(arrayDigitado[ip])
         }else{
-          imprimirCerto(compilado)
-          imprimirErro(arrayTextoDigitado[ip])
+          let armazenado = compilador.join()
+          compilador = []
+          //imprime armazenado como certo
+          
+          //imprime o errado como errado
         }
       }
     }
+    
+
+
   });
 });
 
 
+
+
+
+
+
+
+
+textoDigitado.focus()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var texto = `Fundo para Digitar`
+
+
+var textoPagina = document.getElementById('texto-fundo')
+textoPagina.innerHTML = texto
+
+
+var textoDigitado = document.getElementById('digitado')
+var textoTela = document.getElementById('texto')
+
+
+
+// textoDigitado.addEventListener('input', () => {
+//   textoTela.textContent = textoDigitado.value
+// })
+
+///aqui temos a função que cria os elementos na tela 
+
+
+//esse veio do GPT kkkk
+const inputs = {}
+const inputElements = document.querySelectorAll('input')
+inputElements.forEach(input => {
+  input.addEventListener('input', async () => {
+    textoTela.textContent = textoTela.textContent
+    await new Promise(resolve => setTimeout(resolve, 500));
+    const textoArray = String(input.value).split('') 
+    const textoTelaArray = String(texto).split('')
+    erro = []
+    
+    for (let i = 0 ; textoArray.length > i ; i++) {
+      if (textoArray[i] != textoTelaArray[i] ){
+        erro.push(i)
+      } 
+    } 
+
+    console.log(`
+    Texto digitado:${textoArray}--${textoArray.type}
+    TextoFundo ${textoTelaArray}--${textoTelaArray.type}
+    indice dos erros: ${erro}--${erro.type}
+    `)
+  });
+});
+
+
+
+
+
+
+
+textoDigitado.focus()
+
+
+// armazena na variavel o que ta errado da comparaçõa 
+
+// mo
