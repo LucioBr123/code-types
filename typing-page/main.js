53fa9446358a2d42e50aa9ddfd4f6344c20ef2dc
Aecio_Lucio <aeciofilho12@gmail.com>
2023-04-17 21:50:23 -0300

    adicionando comentando e limpando

--- typing-page/main.js
+++ typing-page/main.js
@@ -4,31 +4,31 @@ let letrasDigitadas = []
 
 
 function escreveCerto(letra) {
-  exibido.innerHTML += letra; 
+  exibido.innerHTML += letra 
 }
 
 function escreveErrado(letra) {
- let span = document.createElement('span');
- span.innerText = letra;
- exibido.appendChild(span);
+ let span = document.createElement('span')
+ span.innerText = letra
+ exibido.appendChild(span)
 }
 
 
 const exibidoFundo = document.getElementById('texto-fundo')
 exibidoFundo.innerHTML = esperado
 
-const input = document.getElementById('digitado');
+const input = document.getElementById('digitado')
 
 input.addEventListener('input', () => {
-  const valor = input.value;
-  letrasDigitadas = valor.split('');
-  exibido.innerHTML = '';
+  const valor = input.value;  // captura o que foi digitado no input
+  letrasDigitadas = valor.split('')  //atribui na o digitado a um array
+  exibido.innerHTML = ''            // limpa o que é exibido
   
   for (let i = 0; i < letrasDigitadas.length; i++) {
     if (letrasDigitadas[i] === esperado[i]) {
-      escreveCerto(letrasDigitadas[i]);
+      escreveCerto(letrasDigitadas[i])  // se certo escreve no html exibido a letrauma por vez comparando no loop se está certa 
     } else {
-      escreveErrado(letrasDigitadas[i]);
+      escreveErrado(letrasDigitadas[i]);// se errada a letra cria um span que vai ficar de cor diferente 
     }
   }
   
