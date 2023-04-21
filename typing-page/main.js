b00917ca91bef075e4fc16ac84005b095ce9284a
Aecio_Lucio <aeciofilho12@gmail.com>
2023-04-20 22:39:19 -0300

    codigo organizado pelo GPT

--- typing-page/main.js
+++ typing-page/main.js
@@ -1,42 +1,42 @@
-const esperado = `Para testar escreva uma letra errada :) Tente escrever abrindo e fechando chaver pra encaixar depois{{(TESTE)}}`
-let exibido = document.getElementById('texto-digitado')
-let letrasDigitadas = []
+// Define o texto esperado e inicializa as variáveis
+const textoEsperado = 'Texto de fundo para teste';
+let elementoTextoExibido = document.getElementById('texto-digitado');
+let letrasDigitadas = [];
 
-
-function escreveCerto(letra) {
-  exibido.innerHTML += letra; 
+// Funções auxiliares para escrever letras corretas e incorretas
+function escreverLetraCorreta(letra) {
+  elementoTextoExibido.innerHTML += letra; 
 }
 
-function escreveErrado(letra) {
- let span = document.createElement('span');
- span.innerText = letra;
- exibido.appendChild(span);
+function escreverLetraIncorreta(letra) {
+  let span = document.createElement('span');
+  span.innerText = letra;
+  elementoTextoExibido.appendChild(span);
 }
 
+// Define o texto esperado no elemento HTML de exibição
+const elementoTextoEsperado = document.getElementById('texto-fundo');
+elementoTextoEsperado.innerHTML = textoEsperado;
 
-const exibidoFundo = document.getElementById('texto-fundo')
-exibidoFundo.innerHTML = esperado
-
-const input = document.getElementById('digitado');
-
-input.addEventListener('input', () => {
-  const valor = input.value;
-  letrasDigitadas = valor.split('');
-  exibido.innerHTML = '';
+// Adiciona um ouvinte de eventos ao elemento de entrada
+const elementoInput = document.getElementById('digitado');
+elementoInput.addEventListener('input', () => {
+  // Obtém as letras digitadas
+  const textoDigitado = elementoInput.value;
+  letrasDigitadas = textoDigitado.split('');
   
+  // Limpa o texto exibido
+  elementoTextoExibido.innerHTML = '';
+  
+  // Escreve as letras corretas e incorretas
   for (let i = 0; i < letrasDigitadas.length; i++) {
-    if (letrasDigitadas[i] === esperado[i]) {
-      escreveCerto(letrasDigitadas[i]);
+    if (letrasDigitadas[i] === textoEsperado[i]) {
+      escreverLetraCorreta(letrasDigitadas[i]);
     } else {
-      escreveErrado(letrasDigitadas[i]);
+      escreverLetraIncorreta(letrasDigitadas[i]);
     }
   }
   
-  // Inserir o texto da função escreveCerto() após a última <span>
-  if (letrasDigitadas.length > 0) {
-    const ultimaSpan = exibido.lastElementChild;
-    ultimaSpan.insertAdjacentHTML('afterend');
-  }
+  // Move o cursor para o campo de entrada
+  elementoInput.focus();
 });
-
-input.focus();
