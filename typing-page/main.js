e9e4b9f5a2892f41d2de644ea99cecd442d7f8b7
Aecio_Lucio <aeciofilho12@gmail.com>
2023-04-20 22:40:45 -0300

    codigo organizado pelo GPT

--- typing-page/main.js
+++ typing-page/main.js
@@ -1,42 +1,42 @@
 // Define o texto esperado e inicializa as variáveis
-const textoEsperado = 'Texto de fundo para teste';
-let elementoTextoExibido = document.getElementById('texto-digitado');
-let letrasDigitadas = [];
+const textoEsperado = 'Texto de fundo para teste'
+let elementoTextoExibido = document.getElementById('texto-digitado')
+let letrasDigitadas = []
 
 // Funções auxiliares para escrever letras corretas e incorretas
 function escreverLetraCorreta(letra) {
-  elementoTextoExibido.innerHTML += letra; 
+  elementoTextoExibido.innerHTML += letra 
 }
 
 function escreverLetraIncorreta(letra) {
-  let span = document.createElement('span');
-  span.innerText = letra;
-  elementoTextoExibido.appendChild(span);
+  let span = document.createElement('span')
+  span.innerText = letra
+  elementoTextoExibido.appendChild(span)
 }
 
 // Define o texto esperado no elemento HTML de exibição
-const elementoTextoEsperado = document.getElementById('texto-fundo');
-elementoTextoEsperado.innerHTML = textoEsperado;
+const elementoTextoEsperado = document.getElementById('texto-fundo')
+elementoTextoEsperado.innerHTML = textoEsperado
 
 // Adiciona um ouvinte de eventos ao elemento de entrada
-const elementoInput = document.getElementById('digitado');
+const elementoInput = document.getElementById('digitado')
 elementoInput.addEventListener('input', () => {
   // Obtém as letras digitadas
-  const textoDigitado = elementoInput.value;
-  letrasDigitadas = textoDigitado.split('');
+  const textoDigitado = elementoInput.value
+  letrasDigitadas = textoDigitado.split('')
   
   // Limpa o texto exibido
-  elementoTextoExibido.innerHTML = '';
+  elementoTextoExibido.innerHTML = ''
   
   // Escreve as letras corretas e incorretas
   for (let i = 0; i < letrasDigitadas.length; i++) {
     if (letrasDigitadas[i] === textoEsperado[i]) {
-      escreverLetraCorreta(letrasDigitadas[i]);
+      escreverLetraCorreta(letrasDigitadas[i])
     } else {
-      escreverLetraIncorreta(letrasDigitadas[i]);
+      escreverLetraIncorreta(letrasDigitadas[i])
     }
   }
   
   // Move o cursor para o campo de entrada
-  elementoInput.focus();
-});
+  elementoInput.focus()
+})
