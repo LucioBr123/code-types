2de26e46941f5d6eb043c27b0a0c4597db42d5be
Aecio_Lucio <aeciofilho12@gmail.com>
2023-04-24 22:54:45 -0300

    Cursor de texto funcionando :)

--- typing-page/main.js
+++ typing-page/main.js
@@ -1,5 +1,5 @@
 // Define o texto esperado e inicializa as variáveis
-const textoEsperado = `Texto de fundo para teste`
+const textoEsperado = `aaaaaaaaaaaaaaaaaaaaaa bbbbbbbbbbbbbbbbbbbbb cccccccccccccccccccccccccc`
 let elementoTextoExibido = document.getElementById('texto-digitado')
 let letrasDigitadas = []
 let cursor = document.getElementById('cursor') // Adiciona uma referência ao elemento do indicador visual do cursor
